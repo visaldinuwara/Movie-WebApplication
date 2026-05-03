27acf7189b346213aff4b4a26ff35d4bf6d3ae15
Visal Dinuwara <visaldinuwara@gmail.com>
2026-05-04 04:17:23 +0530

    Implement movie fetching functionality and enhance search UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useEffect, useState } from 'react'
 import Search from './components/Search'
 import './App.css'
 
 const App = () => {
+  const BASE_URL = "http://www.omdbapi.com/?";
+  const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
 
+  const [errorMessage, setErrorMessage] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+
+  const fetchMovies = async () => {
+    try {
+      const endPoint = `${BASE_URL}s=${searchTerm}&apikey=3704d809`;
+      const response = await fetch(endPoint);
+      if (!response.ok) {
+        throw new Error('Failed to fetch movies');
+      }
+      const data = await response.json();
+      console.log(data); // should show results in console
+      setSearchResults(data.Search || []);
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  };
   return (
     <main>
-      <div className="patter" />
-      <div className="wrapper" />
       <header>
         <img src='./hero-bg.png' alt="Hero Banner"/>
         <h1 className="text-3xl font-bold underline">
           Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle
         </h1>
       </header>
-      <Search searchTerm={searchResults} setSearchTerm={setSearchResults}/>
+      <div className='search_div'>
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+      <button onClick={fetchMovies} className='fetch-button'>Fetch Movies</button>
+      </div>
+
+      <section className="all-movies">
+        <h2>All Movies</h2>
+        {errorMessage && <p className="error">{errorMessage}</p>}
+        <ul>
+          {searchResults.map(movie => (
+            <li key={movie.imdbID}>{movie.Title} ({movie.Year})</li>
+          ))}
+        </ul>
+      </section>
     </main>
   )
 }
